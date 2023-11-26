3f3e2a3240554332e3bc18e7e0f44563f643a05d
yutounun <geek.yuto@gmail.com>
2023-11-26 09:56:29 +0900

    update sidebar row style

--- src/app/_commons/SidebarRow.tsx
+++ src/app/_commons/SidebarRow.tsx
@@ -1,11 +1,38 @@
+"use client";
 import React from "react";
 import { Stack, Typography } from "../../../node_modules/@mui/material/index";
+import { usePathname } from "../../../node_modules/next/navigation";
 
 interface propTypes {
   children: React.ReactNode;
   title: string;
 }
 const SidebarRow = ({ children, title }: propTypes) => {
+  const currentPath = usePathname();
+
+  /**
+   * Determines if the title and path match certain conditions and returns a boolean value.
+   *
+   * @return {boolean} - true if the title and path match certain conditions, false otherwise.
+   */
+  function isBold() {
+    if (title === "作成" && currentPath === "/shops/create") {
+      return true;
+    }
+    if (title === "検索" && currentPath === "/shops/search") {
+      return false;
+    }
+    if (title === "一覧" && currentPath === "/shops/list") {
+      return true;
+    }
+    if (title === "プロフィール" && currentPath === "/profile") {
+      return true;
+    }
+    if (title === "設定" && currentPath === "/config") {
+      return true;
+    }
+    return false;
+  }
   return (
     <Stack
       direction="row"
@@ -24,7 +51,7 @@ const SidebarRow = ({ children, title }: propTypes) => {
       {/* Icon */}
       {children}
 
-      <Typography variant="h6">{title}</Typography>
+      <Typography fontWeight={isBold() ? "bold" : "normal"}>{title}</Typography>
     </Stack>
   );
 };
