6e864ce72d73b87d8835e67e634a4994a5971622
yutounun <geek.yuto@gmail.com>
2023-11-29 23:30:39 +0900

    use json-server to mock get api

--- src/app/cafe/list/page.tsx
+++ src/app/cafe/list/page.tsx
@@ -1,26 +1,33 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { CafeI } from "types/cafes";
 import "./cafe-list.scss";
 import CafeRow from "./CafeRow";
-import Data from "./data.json";
 import StationSearch from "./StationSearch";
 
 const ShopsList = () => {
+  const [cafeList, setCafeList] = useState<CafeI[]>([]);
+  useEffect(() => {
+    fetch("http://localhost:9000/cafe")
+      .then((res) => res.json())
+      .then((json) => {
+        setCafeList(json);
+        console.log(json);
+      })
+      .catch(() => error("error"));
+  }, []);
+
   function filterEbisuAndMeguro() {
-    const filteredData = Data.filter((cafe) => {
-      if (cafe.area.includes("目黒") || cafe.area.includes("銀座")) {
-        return cafe;
-      }
-    });
+    const filteredData = cafeList;
     return filteredData;
   }
   return (
     <>
       <StationSearch />
       <CafeRow area="恵比寿・目黒周辺" cafes={filterEbisuAndMeguro()} />
-      <CafeRow area="銀座・新橋周辺" cafes={Data} />
-      <CafeRow area="表参道・青山周辺" cafes={Data} />
+      <CafeRow area="銀座・新橋周辺" cafes={cafeList} />
+      <CafeRow area="表参道・青山周辺" cafes={cafeList} />
     </>
   );
 };
