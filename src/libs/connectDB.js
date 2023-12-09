7dae89b62d45bbc487486601ee550d895fe650e2
yutounun <geek.yuto@gmail.com>
2023-12-09 20:50:54 +0900

    call db only connection is not published

--- src/libs/connectDB.js
+++ src/libs/connectDB.js
@@ -1,12 +1,23 @@
 import mongoose from "mongoose";
 
+/**
+ * Connects to the MongoDB database only when connection is not established.
+ *
+ * @return {Promise<void>} - A promise that resolves once the connection is established.
+ * @throws {Error} - If the connection to the database fails.
+ */
 const connectDB = async () => {
+  if (mongoose.connection.readyState) {
+    console.log("Already connected to MongoDB");
+    return;
+  }
+
   try {
     await mongoose.connect(process.env.DB_API_KEY);
     console.log("Successfully connected to MongoDB");
   } catch (err) {
     console.log("Failed to connect to MongoDB");
-    throw new Error();
+    throw new Error(err.message);
   }
 };
 
