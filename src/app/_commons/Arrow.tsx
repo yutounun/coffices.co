d7cef5586eddf32e0f59c9a3b02b4d0262e6ccae
yutounun <geek.yuto@gmail.com>
2023-12-17 11:05:21 +0900

    set scroll bar on left and right of each row

---
+++ src/app/_commons/Arrow.tsx
@@ -0,0 +1,37 @@
+import { IconButton } from "@mui/material";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+const Arrow = ({
+  direction,
+  onClick,
+  hidden,
+}: {
+  direction: "left" | "right";
+  onClick: () => void;
+  hidden: boolean;
+}) => {
+  const isLeft = direction === "left";
+
+  return (
+    <IconButton
+      onClick={onClick}
+      sx={{
+        color: hidden ? "white" : "black",
+        background: "rgba(255, 255, 255, 0.7)",
+        borderRadius: "50%",
+        padding: "10px",
+        transform: "translateY(-50%)",
+        zIndex: 10,
+        "&:hover": {
+          backgroundColor: "rgba(255, 255, 255, 0.9)",
+          transition: "background-color 0.3s",
+        },
+        // 左側の矢印の場合は左に、そうでない場合は右に配置
+        [isLeft ? "left" : "right"]: "10px",
+      }}
+    >
+      {isLeft ? <ArrowBackIosIcon /> : <ArrowForwardIosIcon />}
+    </IconButton>
+  );
+};
+export default Arrow;
--- src/app/cafe/list/CafeRow.tsx
+++ src/app/cafe/list/CafeRow.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CafeI } from "types/cafes";
 import {
+  IconButton,
   Stack,
   Typography,
 } from "../../../../node_modules/@mui/material/index";
 import CafeCard from "./CafeCard";
 import Loading from "../../loading";
+import Arrow from "../../_commons/Arrow";
 
 interface propTypes {
   cafes: CafeI[];
@@ -14,6 +16,50 @@ interface propTypes {
 }
 
 const CafeRow = ({ cafes, area, isLoading }: propTypes) => {
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const [isScrollLeft, setIsScrollLeft] = useState(false);
+  const [isScrollRight, setIsScrollRight] = useState(true);
+  const [showScroll, setShowScroll] = useState(true);
+
+  const checkScrollPosition = () => {
+    if (containerRef.current) {
+      const { scrollLeft, scrollWidth, clientWidth } = containerRef.current;
+      setIsScrollLeft(scrollLeft > 0);
+      setIsScrollRight(scrollLeft < scrollWidth - clientWidth - 1);
+    }
+  };
+
+  const scroll = (scrollOffset: number) => {
+    if (containerRef.current) {
+      containerRef.current.scrollLeft += scrollOffset;
+      checkScrollPosition();
+    }
+  };
+
+  useEffect(() => {
+    console.log("cafes.length :", cafes.length);
+    if (cafes.length < 6) {
+      setShowScroll(false);
+    } else {
+      setShowScroll(true);
+    }
+    checkScrollPosition(); // 初期位置をチェック
+    setIsScrollRight(true);
+    const handleScroll = () => {
+      checkScrollPosition();
+    };
+
+    const currentContainer = containerRef.current;
+    if (currentContainer) {
+      currentContainer.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (currentContainer) {
+        currentContainer.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, [cafes.length]);
   return (
     <>
       <Typography
@@ -29,11 +75,33 @@ const CafeRow = ({ cafes, area, isLoading }: propTypes) => {
       >
         {area}
       </Typography>
-      <Stack direction="row" spacing={3} className="row__cards">
-        {isLoading && <Loading />}
-        {cafes &&
-          cafes.length > 0 &&
-          cafes.map((cafe) => <CafeCard key={cafe._id} cafe={cafe} />)}
+
+      <Stack
+        direction="row"
+        spacing={3}
+        sx={{ height: "30em", alignItems: "center", justifyContent: "center" }}
+      >
+        <Arrow
+          hidden={!isScrollLeft || !showScroll}
+          direction="left"
+          onClick={() => scroll(-1000)}
+        />
+        <Stack
+          ref={containerRef}
+          direction="row"
+          spacing={3}
+          className="row__cards"
+        >
+          {isLoading && <Loading />}
+          {cafes &&
+            cafes.length > 0 &&
+            cafes.map((cafe) => <CafeCard key={cafe._id} cafe={cafe} />)}
+        </Stack>
+        <Arrow
+          hidden={!isScrollRight || !showScroll}
+          direction="right"
+          onClick={() => scroll(1000)}
+        />
       </Stack>
     </>
   );
