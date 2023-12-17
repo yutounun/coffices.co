9e0d787fa69a344712fe3a11f04135a04a71f465
yutounun <geek.yuto@gmail.com>
2023-12-18 07:43:36 +0900

    fix cafeShopsInSpecificArea to return valid result

--- src/app/(Header)/cafe/list/page.tsx
+++ src/app/(Header)/cafe/list/page.tsx
@@ -1,19 +1,17 @@
 "use client";
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useCallback, useEffect, useState } from "react";
 import Loading from "../../../loading";
 import { useQuery } from "react-query";
 import { CafeI } from "types/cafes";
-import { filterCafe, getCafe } from "_utils/api";
+import { getCafe } from "_utils/api";
 import "../../../styles/cafe-list.scss";
 import CafeRow from "./CafeRow";
-import useCreateModalStore from "../../../../store/openCreateCafeModal";
 import { CafeListContext } from "../../../../contexts/CafeListContext";
 import CafeSearchList from "./CafeSearchList";
 import { StationNameContext } from "../../../../contexts/StationNameContext";
 
 const ShopsList = () => {
   const { stationName } = React.useContext(StationNameContext);
-  const { showsCreateModal, closeCreateCafeModal } = useCreateModalStore();
   const [cafeList, setCafeList] = useState<CafeI[]>([]);
 
   const { data, isLoading } = useQuery("cafes", getCafe);
@@ -23,23 +21,36 @@ const ShopsList = () => {
       setCafeList(data);
     }
   }, [data]);
-  function cafeShopsInSpecificArea(stations: string[]) {
-    return cafeList?.filter((cafe) => {
-      for (const station of stations) {
-        return cafe.station === station;
-      }
-    });
-  }
 
-  function filteredCafes() {
+  /**
+   *  Find cafe in specific area
+   *
+   * @param stations station name
+   * @returns filtered cafe
+   */
+  const cafeShopsInSpecificArea = useCallback(
+    (stations: string[]) => {
+      return cafeList.filter((cafe) =>
+        stations.some((station) => cafe.station === station)
+      );
+    },
+    [cafeList]
+  );
+
+  /**
+   * Find cafe in specific area
+   *
+   * @return filtered cafe by station name
+   */
+  const filteredCafes = useCallback(() => {
     return cafeList?.filter((cafe) => {
       return cafe.station === stationName;
     });
-  }
+  }, [cafeList, stationName]);
 
-  function top20() {
+  const top20 = useCallback(() => {
     return cafeList?.slice(0, 20);
-  }
+  }, [cafeList]);
 
   return (
     <>
