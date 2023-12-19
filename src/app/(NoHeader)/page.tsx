027bab3a9c7e4abf21f76a9dd23df4f44193a9a7
yutounun <geek.yuto@gmail.com>
2023-12-20 07:58:31 +0900

    remove wasted space on top on mobile login page style

--- src/app/(NoHeader)/page.tsx
+++ src/app/(NoHeader)/page.tsx
@@ -22,7 +22,6 @@ export default function Home() {
         <Stack
           sx={{
             width: "100%",
-            height: "100vh",
             backgroundColor: "white",
             py: "2em",
             justifyContent: "center",
