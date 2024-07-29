dc2371a74d60024bd15bb571eae13ef3c333d6a0
yutounun <geek.yuto@gmail.com>
2024-07-29 21:55:20 +0900

    update all variant in theme.ts

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -3,26 +3,75 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
-    primary: {
-      main: "#6B4E31",
-      light: "#9E8B7D",
-      dark: "#513C2C",
-      contrastText: "white",
-    },
-    // accent color
-    secondary: {
-      main: "#C06014",
-    },
     mode: "light", // Sets the theme to light mode
+    primary: {
+      main: "#FFD8B5",
+      light: "#FF8F2A",
+      dark: "#FF8F2A",
+      contrastText: "#25313C",
+    },
+    secondary: {
+      dark: "#FF8F2A",
+      main: "#FFD8B5",
+      light: "#FF8F2A",
+    },
+    error: {
+      main: "#FF8F2A",
+      contrastText: "#FFFFFF",
+    },
+    warning: {
+      main: "#FFD8B5",
+      contrastText: "#25313C",
+    },
+    info: {
+      main: "#DAE3EA",
+      contrastText: "#25313C",
+    },
+    success: {
+      main: "#D2D2D2",
+      contrastText: "#25313C",
+    },
   },
+
   typography: {
-    fontFamily: [
-      "Roboto",
-      '"Noto Sans JP"',
-      '"Helvetica"',
-      "Arial",
-      "sans-serif",
-    ].join(","),
+    h1: {
+      fontFamily:
+        "Lato, 'Open Sans', Roboto, 'Noto Sans JP', 'Helvetica', Arial, sans-serif",
+      fontSize: "3rem", // 48px
+    },
+    h2: {
+      fontFamily:
+        "Lato, 'Open Sans', Roboto, 'Noto Sans JP', 'Helvetica', Arial, sans-serif",
+      fontSize: "2rem", // 32px
+    },
+    h3: {
+      fontFamily:
+        "Lato, 'Open Sans', Roboto, 'Noto Sans JP', 'Helvetica', Arial, sans-serif",
+      fontSize: "1.5rem", // 24px
+    },
+    h4: {
+      fontFamily:
+        "Lato, 'Open Sans', Roboto, 'Noto Sans JP', 'Helvetica', Arial, sans-serif",
+      fontSize: "1rem", // 16px
+    },
+    caption: {
+      fontFamily:
+        "Open Sans, Roboto, 'Noto Sans JP', 'Helvetica', Arial, sans-serif",
+      fontSize: "0.625rem", // 10px
+      lineHeight: 1.5, // 150.6%
+    },
+    body1: {
+      fontFamily:
+        "Open Sans, Roboto, 'Noto Sans JP', 'Helvetica', Arial, sans-serif",
+      fontSize: "1.125rem", // 18px
+      lineHeight: 1.63, // 163.2%
+    },
+    button: {
+      fontFamily:
+        "Open Sans, Roboto, 'Noto Sans JP', 'Helvetica', Arial, sans-serif",
+      fontSize: "1.125rem", // 18px
+      lineHeight: 1.63, // 163.2%
+    },
   },
 });
 
