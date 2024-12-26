e298652b62c2d7f4df74796e147489acf93c2c10
yutounun <geek.yuto@gmail.com>
2024-12-25 22:43:28 -0800

    update height of review cards

--- src/app/[locale]/search/[placeId]/UsedReview.tsx
+++ src/app/[locale]/search/[placeId]/UsedReview.tsx
@@ -10,11 +10,11 @@ const UsedReview = ({ review, index }: { review: string; index: number }) => {
         px: 4,
         py: 2,
         backgroundColor: "white",
-        minWidth: "300px",
-        maxWidth: "400px",
-        minHeight: "150px",
+        minWidth: "200px",
+        maxWidth: "300px",
         boxShadow: "0px 2px 5px rgba(0, 0, 0, 0.1)",
         borderRadius: 2,
+        height: "fit-content",
       }}
     >
       <Typography variant="body1" sx={{ whiteSpace: "normal" }}>
--- src/app/[locale]/search/[placeId]/UsedReviews.tsx
+++ src/app/[locale]/search/[placeId]/UsedReviews.tsx
@@ -5,13 +5,16 @@ import UsedReview from "./UsedReview";
 const UsedReviews = ({ detailInfo }: { detailInfo?: CafeAnalysisI }) => {
   return (
     <Stack direction="column" gap={2} sx={{ pt: 2, pb: 6 }}>
-      <Typography variant="h2">Reviews used by AI</Typography>
+      <Typography variant="h2">
+        Reviews used by AI ({detailInfo?.important_reviews.length})
+      </Typography>
       <Stack
         direction="row"
         gap={3}
         sx={{
           overflowX: "auto",
           px: 1,
+          py: 1,
         }}
       >
         {detailInfo?.important_reviews.map((review, index) => (
